d3ea53ceceded35a7333b9b9cf12ad23727e2167
thebigcoom8 <thebigcoom8@gmail.com>
2026-03-06 11:29:45 -0600

    add the website

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'gambits-site-v1';
+const ASSETS = [
+  '/index.html',
+  '/blogpostpage.html',
+  '/postforge.html',
+  '/header.html',
+  '/style.css',
+  '/neocities.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/marked/9.1.6/marked.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      // Cache any new pages visited on the fly
+      if (e.request.destination === 'document') {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
